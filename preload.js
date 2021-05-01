1dd88e0d208e34ab7efb6627c71a471bcd109686
mythicalbadger <jussy12.work@gmail.com>
2021-05-01 08:56:32 +0700

    Added type of data selection and arrows for case increase/decrease
    You can now choose what type of data (cases, total cases, deaths, total deaths) you want to display on the graph.
    
    Also, there are arrows so you know if the cases/deaths have gone up or down.

--- preload.js
+++ preload.js
@@ -15,7 +15,85 @@ window.addEventListener('DOMContentLoaded', () => {
             for (var i = 0; i < IDList.length; i++) {
                 replaceText(IDList[i], json[APIList[i]]);
             }
-            
+
+            fetch("https://covid19.th-stat.com/api/open/timeline")
+            .then(result => result.json())
+            .then(json => {
+
+                var latestCases = json["Data"][json["Data"].length - 1]["NewConfirmed"];
+                var yesterdayCases = json["Data"][json["Data"].length - 2]["NewConfirmed"];
+                var latestDeaths = json["Data"][json["Data"].length - 1]["NewDeaths"];
+                var yesterdayDeaths = json["Data"][json["Data"].length - 2]["NewDeaths"];
+                
+                if (latestCases > yesterdayCases) {
+                    if (document.getElementById("case-arrow").classList.contains("fa-chevron-down")) {
+                        document.getElementById("case-arrow").classList.remove("fa-chevron-down")
+                        document.getElementById("death-arrow").classList.remove("red")
+                    }
+                    else if (document.getElementById("case-arrow").classList.contains("fa-equals")) {
+                        document.getElementById("case-arrow").classList.remove("fa-equals")
+                    }
+                    document.getElementById("case-arrow").classList.toggle("fa-chevron-up")
+                    document.getElementById("case-arrow").classList.toggle("green")
+                }
+                else if (latestCases == yesterdayCases) {
+                    if (document.getElementById("case-arrow").classList.contains("fa-chevron-down")) {
+                        document.getElementById("case-arrow").classList.remove("fa-chevron-down")
+                        document.getElementById("case-arrow").classList.remove("red")
+                    }
+                    else if (document.getElementById("case-arrow").classList.contains("fa-chevron-up")) {
+                        document.getElementById("case-arrow").classList.remove("fa-chevron-up")
+                        document.getElementById("case-arrow").classList.remove("green")
+                    }
+                    document.getElementById("case-arrow").classList.toggle("fa-equals")
+                }
+                else {
+                    if (document.getElementById("case-arrow").classList.contains("fa-chevron-up")) {
+                        document.getElementById("case-arrow").classList.remove("fa-chevron-up")
+                        document.getElementById("death-arrow").classList.remove("green")
+                    }
+                    else if (document.getElementById("case-arrow").classList.contains("fa-equals")) {
+                        document.getElementById("case-arrow").classList.remove("fa-equals")
+                    }
+                    document.getElementById("case-arrow").classList.toggle("fa-chevron-down")
+                    document.getElementById("case-arrow").classList.toggle("red")
+                }
+
+                if (latestDeaths > yesterdayDeaths) {
+                    if (document.getElementById("death-arrow").classList.contains("fa-chevron-down")) {
+                        document.getElementById("death-arrow").classList.remove("fa-chevron-down")
+                        document.getElementById("death-arrow").classList.remove("red")
+                    }
+                    else if (document.getElementById("death-arrow").classList.contains("fa-equals")) {
+                        document.getElementById("death-arrow").classList.remove("fa-equals")
+                    }
+                    document.getElementById("death-arrow").classList.toggle("fa-chevron-up")
+                    document.getElementById("death-arrow").classList.toggle("green")
+                }
+                else if (latestDeaths == yesterdayDeaths) {
+                    if (document.getElementById("death-arrow").classList.contains("fa-chevron-down")) {
+                        document.getElementById("death-arrow").classList.remove("fa-chevron-down")
+                        document.getElementById("death-arrow").classList.remove("red")
+                    }
+                    else if (document.getElementById("death-arrow").classList.contains("fa-chevron-up")) {
+                        document.getElementById("death-arrow").classList.remove("fa-chevron-up")
+                        document.getElementById("death-arrow").classList.remove("green")
+                    }
+                    document.getElementById("death-arrow").classList.toggle("fa-equals")
+                }
+                else {
+                    if (document.getElementById("death-arrow").classList.contains("fa-chevron-up")) {
+                        document.getElementById("death-arrow").classList.remove("fa-chevron-up")
+                        document.getElementById("death-arrow").classList.remove("green")
+                    }
+                    else if (document.getElementById("death-arrow").classList.contains("fa-equals")) {
+                        document.getElementById("death-arrow").classList.remove("fa-equals")
+                    }
+                    document.getElementById("death-arrow").classList.toggle("fa-chevron-down")
+                    document.getElementById("death-arrow").classList.toggle("red")
+                }
+            });
+
     });
 
 });
